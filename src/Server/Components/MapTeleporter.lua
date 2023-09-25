c0da38d24e971af10e504409a6f55b2f10d74299
AlphaRunic <roleyspersonal@gmail.com>
2023-09-25 00:14:53 -0400

    feat: add back teleporters

--- src/Server/Components/MapTeleporter.lua
+++ src/Server/Components/MapTeleporter.lua
@@ -23,12 +23,19 @@ local MapTeleporter: Component.Def = {
       RequiredWins = { Type = "number" },
     },
     Children = {
-      Portal = { ClassName = "MeshPart" }
+      Portal = { ClassName = "MeshPart" },
+      Back = {
+        ClassName = "Model",
+        Children = {
+          Circle = { ClassName = "MeshPart" }
+        }
+      }
     }
   };
 }
 
-local playerDebounces: { [number]: typeof(Debounce.new(0)) } = {}
+local playerTeleporterDebounces: { [number]: typeof(Debounce.new(0)) } = {}
+local playerBackTeleporterDebounces: { [number]: typeof(Debounce.new(0)) } = {}
 function MapTeleporter:Initialize(): nil
   self._data = Knit.GetService("DataService")
 
@@ -40,25 +47,52 @@ function MapTeleporter:Initialize(): nil
   local destinationCFrame = (destinationTeleporter.PrimaryPart :: BasePart).CFrame
   local destinationOffset = destinationCFrame.LookVector * 5
 
-  self:AddToJanitor(self.Instance.Portal.Touched:Connect(function(hit: BasePart)
-    if self._mapName == "Map3" then return end
+  local previousMapName = `Map{tonumber(mapNumberString) :: number - 1}`
+  local previousMap = workspace:FindFirstChild(previousMapName) :: Model
+  local previousTeleporter = previousMap:FindFirstChild("Teleporter") :: Model
+  local previousCFrame = (previousTeleporter.PrimaryPart :: BasePart).CFrame
+  local previousOffset = previousCFrame.LookVector * 5
 
+  local function getPlayerFromPart(hit: BasePart): Player?
     local character = hit:FindFirstAncestorOfClass("Model")
     if not character then return end
     local humanoid = character:FindFirstChildOfClass("Humanoid")
     if not humanoid then return end
-    local player = Players:GetPlayerFromCharacter(character)
+    return Players:GetPlayerFromCharacter(character)
+  end
+
+  self:AddToJanitor(self.Instance.Portal.Touched:Connect(function(hit: BasePart)
+    if self._mapName == "Map3" then return end
+
+    local player = getPlayerFromPart(hit)
     if not player then return end
-    if not playerDebounces[player.UserId] then
-      local db = Debounce.new(3)
-      playerDebounces[player.UserId] = db
+    if not playerTeleporterDebounces[player.UserId] then
+      local db = Debounce.new(2)
+      playerTeleporterDebounces[player.UserId] = db
       self:AddToJanitor(db)
     end
     
-    local db = playerDebounces[player.UserId]
+    local db = playerTeleporterDebounces[player.UserId]
     if db:IsActive() then return end
-    self:Teleport(character, destinationCFrame + destinationOffset)
+    self:Teleport(player.Character, destinationCFrame + destinationOffset)
   end))
+
+  self:AddToJanitor(self.Instance.Back.Circle.Touched:Connect(function(hit: BasePart)
+    if self._mapName == "Map1" then return end
+
+    local player = getPlayerFromPart(hit)
+    if not player then return end
+    if not playerBackTeleporterDebounces[player.UserId] then
+      local db = Debounce.new(2)
+      playerBackTeleporterDebounces[player.UserId] = db
+      self:AddToJanitor(db)
+    end
+    
+    local db = playerBackTeleporterDebounces[player.UserId]
+    if db:IsActive() then return end
+    self:Teleport(player.Character, previousCFrame + previousOffset)
+  end))
+
   return
 end
 
@@ -66,7 +100,8 @@ function MapTeleporter:Teleport(character: Model, cframe: CFrame): nil
   if not Array.new(self._data:GetValue("DefeatedBosses")):Has(self._mapName) then return end
   if self._data:GetValue("Rebirths") < self.Attributes.RequiredRebirths then return end
   if self._data:GetValue("Wins") < self.Attributes.RequiredWins then return end
-  return character:PivotTo(cframe)
+  (character.PrimaryPart :: BasePart).CFrame = cframe
+  return
 end
 
 return Component.new(MapTeleporter)
