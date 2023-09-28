f2b398e7f18be5e0d4720f6d8f9dc8319437bae1
AlphaRunic <roleyspersonal@gmail.com>
2023-09-27 22:12:03 -0400

    feat: create SendTradeScreen component

---
+++ src/Client/Components/UI/Screens/SendTradeScreen.lua
@@ -0,0 +1,66 @@
+--!native
+--!strict
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+
+local Packages = ReplicatedStorage.Packages
+local Knit = require(Packages.Knit)
+local Component = require(Packages.Component)
+local Array = require(Packages.Array)
+
+local player = Players.LocalPlayer
+
+local SendTradeScreen: Component.Def = {
+	Name = script.Name;
+	IgnoreAncestors = { StarterGui };
+	Guards = {
+		Ancestors = { player.PlayerGui },
+		Name = "TradeSend",
+		ClassName = "ScreenGui",
+		Children = {
+			Background = {
+				ClassName = "ImageLabel",
+				Children = {
+					Close = { ClassName = "ImageButton" },
+					Players = { ClassName = "ScrollingFrame" }
+				}
+			}
+		}
+	};
+}
+
+function SendTradeScreen:Initialize(): nil
+	self._data = Knit.GetService("DataService")
+	
+  local playerFrameTemplate = ReplicatedStorage.Assets.UserInterface.Trading.Player
+  local playerContainer = self.Instance.Background.Players
+	self:AddToJanitor(Players.PlayerAdded:Connect(function(newPlayer: Player): nil
+    if newPlayer == player then return end
+
+		local playerFrame = playerFrameTemplate:Clone()
+    playerFrame.Name = tostring(newPlayer.UserId)
+    playerFrame.NameLabel.Text = newPlayer.DisplayName
+    playerFrame.Send.MouseButton1Click:Connect(function(): nil
+      -- send a trade to `player`
+      return
+    end)
+    return
+	end))
+  self:AddToJanitor(Players.PlayerRemoving:Connect(function(leavingPlayer: Player): nil
+    if leavingPlayer == player then return end
+		local playerFrame = Array.new(playerContainer:GetChildren())
+      :Find(function(playerFrame)
+        return playerFrame.Name == tostring(leavingPlayer.UserId)
+      end)
+
+    if playerFrame then
+      playerFrame:Destroy()
+    end
+    return
+	end))
+  
+	return
+end
+
+return Component.new(SendTradeScreen)
